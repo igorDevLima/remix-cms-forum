11e2f684ed68192d5dc6243e8868c526d97e520b
IgorLima <igorlimadrum@gmail.com>
2024-02-22 18:33:11 -0300

    feat: implement navsidebar

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -11,7 +11,17 @@ import {
 
 import style from "./app.css"
 
+//images and icons
+import redditLogoImage from "../public/images/Logo_reddit.svg"
+import iconHome from "../public/icons/icon-home.svg"
+import iconHot from "../public/icons/icon-hot.svg"
+import iconTrending from "../public/icons/icon-trending.svg"
+
+import { SidebarNavItem, links as SidebarNavItemLinks } from "./components/SidebarNavItem";
+
+
 export const links: LinksFunction = () => [
+  ...SidebarNavItemLinks(),
   { rel: "stylesheet", href: style }
 ];
 
@@ -25,7 +35,31 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div className="container">
+          <div id="sidebar">
+            <img src={redditLogoImage} alt="Reddit logo" />
+            <div className="sidebar-nav">
+              <nav>
+                <h2>feeds</h2>
+                <SidebarNavItem iconUrl={iconHome} label="Home" to="/" />
+                <SidebarNavItem iconUrl={iconHot} label="Hot" to="/" />
+                <SidebarNavItem iconUrl={iconTrending} label="Recent" to="/" />
+              </nav>
+              <nav>
+                <h2>My communities</h2>
+                <SidebarNavItem iconUrl={iconHome} label="Home" to="/" />
+                <SidebarNavItem iconUrl={iconHot} label="Hot" to="/" />
+                <SidebarNavItem iconUrl={iconTrending} label="Recent" to="/" />
+              </nav>
+            </div>
+          </div>
+          <main>
+            <header className="main-header"></header>
+            <div className="main-content">
+              <Outlet />
+            </div>
+          </main>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
